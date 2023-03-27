a5e3eba29b3ef322292f53a56afcaaa685df4a52
RpThiagoluiz <rp.thiagoluiz@gmail.com>
2023-03-26 22:08:37 -0300

    feat: controll navbar hook and refactor code

--- app/src/components/AppHeader/index.tsx
+++ app/src/components/AppHeader/index.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useState } from 'react';
 import Image from 'next/image';
 import { Wrapper } from '@/layout/Wrapper';
 import logoImg from '@/assets/images/logo.svg';
@@ -9,11 +9,12 @@ import { VscChromeClose } from 'react-icons/vsc';
 import Link from 'next/link';
 import { Navigation } from './components/Navigation';
 import { MobileNavigation } from './components/MobileNavigation';
+import { useControlNavBar } from './hook/useControlNavBar';
 
 export function AppHeader() {
-  const [show, setShow] = useState('translate-y-0');
-  const [lastScrollY, setLastScrollY] = useState(0);
   const [mobileMenu, setMobileMenu] = useState(false);
+
+  const { showNavBar } = useControlNavBar({ mobileMenu });
 
   const cartItems = [1, 2, 3, 5, 6, 7, 8];
 
@@ -21,29 +22,9 @@ export function AppHeader() {
     setMobileMenu((prevState) => !prevState);
   };
 
-  const controlNavbar = useCallback(() => {
-    if (window.scrollY > 200) {
-      if (window.scrollY > lastScrollY && !mobileMenu) {
-        setShow('-translate-y-[80px]');
-      } else {
-        setShow('shadow-sm');
-      }
-    } else {
-      setShow('translate-y-0');
-    }
-    setLastScrollY(window.scrollY);
-  }, [lastScrollY, mobileMenu]);
-
-  useEffect(() => {
-    window.addEventListener('scroll', controlNavbar);
-    return () => {
-      window.removeEventListener('scroll', controlNavbar);
-    };
-  }, [lastScrollY, controlNavbar]);
-
   return (
     <nav
-      className={`w-full h-[50px] md:h-[80px] bg-white flex items-center justify-between z-20 stick top-0 transition-transform duration-300 ${show}`}
+      className={`w-full h-[50px] md:h-[80px] bg-white flex items-center justify-between z-20 sticky top-0 transition-transform duration-300 ${showNavBar}`}
     >
       <Wrapper className="h-[60px] flex items-center justify-between">
         <Link href="/">
@@ -59,7 +40,6 @@ export function AppHeader() {
         )}
 
         <div className="flex items-center gap-2 text-black">
-          {/* Icon start */}
           <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
             <IoMdHeartEmpty className="text-[19px] md:text-[24px]" />
             <div className="h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px]">
--- app/src/pages/home/index.tsx
+++ app/src/pages/home/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <>
+    <div className="bg-purple-900 h-[1850px]">
       <h1>HOME</h1>
-    </>
+    </div>
   );
 }
